c8b855599546bac52293a4bfc3806b73d344b959
F2Pl4y <91162391+F2Pl4y@users.noreply.github.com>
2024-01-03 17:55:52 -0500

    .

--- js/prueba.js
+++ js/prueba.js
@@ -101,9 +101,13 @@ function initMap() {
             var address = "";
             if (place.address_components) {
                 address = [
-
-                ]
+                    (place.address_components[0] && place.address_components[0].short_name || ''),
+                    (place.address_components[1] && place.address_components[1].short_name || ''),
+                    (place.address_components[2] && place.address_components[2].short_name || ''),
+                ];
             }
+            informacion.setContent('<div>' + place.name + ' </div><br>' + address);
+            informacion.open(map, marcador);
         });
 
     });
